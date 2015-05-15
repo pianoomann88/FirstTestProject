668fa229d30459f3dfd37144c2278a1ac705a7d5
Lee Huff <Home@Lees-Mac-Pro.local>
2015-05-14 21:42:59 -0700

    first rev

--- Challenge 1/ViewController.swift
+++ Challenge 1/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendButtonTouch(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = messageTextField.text
+        messageLabel.textColor = UIColor.greenColor()
+        
+        messageTextField.text = ""
+        messageTextField.resignFirstResponder()
+        
+        nameLabel.hidden = false
+        nameLabel.text = nameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        nameTextField.text = ""
+        nameTextField.resignFirstResponder()
+        
+        sendButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+        
+    }
 
 }
 
